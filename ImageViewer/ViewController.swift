98d440db869f3ea12bb96f9d1b2b1f951008a388
Vivek Gupta <vivek.gupta@raweng.com>
2018-02-03 21:31:41 +0530

    Initial Commit

---
+++ ImageViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by Vivek Narsingh Gupta on 03/02/18.
+//  Copyright © 2018 Vivek Narsingh Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
